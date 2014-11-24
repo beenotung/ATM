4fb136a5fd44da02952e1c2d0c412c166ad71f98
Beeno Tung <aabbcc1241@yahoo.com.hk>
2014-11-25 02:56:34 +0800

    added banner

--- src/atm/gui/mainscreen/Welcome.java
+++ src/atm/gui/mainscreen/Welcome.java
@@ -1,13 +1,40 @@
 package atm.gui.mainscreen;
 
 import java.awt.CardLayout;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+import atm.utils.MyStrings;
+import atm.utils.MyURLs;
+import java.awt.Label;
+import javax.swing.BoxLayout;
+import java.awt.Component;
 
 public class Welcome extends JPanel {
 	private CardLayout cardLayout;
 
 	public Welcome(CardLayout cardLayout) {
 		this.cardLayout = cardLayout;
+
+		try {
+			JLabel lblBanner;
+			setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+			lblBanner = new JLabel(new ImageIcon(new URL(
+					MyURLs.STRING_URL_IMAGE_BANNER)));
+			lblBanner.setAlignmentY(Component.TOP_ALIGNMENT);
+			lblBanner.setAlignmentX(Component.CENTER_ALIGNMENT);
+			add(lblBanner);
+		} catch (MalformedURLException e) {
+			System.out.println(MyStrings.INTERNET_ERROR);
+			JOptionPane.showMessageDialog(getParent(),
+					MyStrings.INTERNET_ERROR, "Internet Error",
+					JOptionPane.ERROR_MESSAGE);
+		}
+
 	}
 }
--- src/atm/utils/MyURLs.java
+++ src/atm/utils/MyURLs.java
@@ -3,4 +3,5 @@ package atm.utils;
 public class MyURLs {
 	public static final String STRING_URL_IMAGE_TRIANGLE_POINT_LEFT = "http://aabbcc1241.freeiz.com/HKCC/oop/publish/images/triangle_point_left.png";
 	public static final String STRING_URL_IMAGE_TRIANGLE_POINT_RIGHT = "http://aabbcc1241.freeiz.com/HKCC/oop/publish/images/triangle_point_right.png";
+	public static final String STRING_URL_IMAGE_BANNER = 	"http://aabbcc1241.freeiz.com/HKCC/oop/publish/images/banner.png";
 }
